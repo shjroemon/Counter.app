06798220cedf823e1f25fd231c2f98d3580c3ebc
shjroemon <zzdaotuanhuyzz@gmail.com>
2023-06-04 17:54:14 +0700

    Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [count, setCount] = useState(1);
+    let color;
+    if (count > 0) {
+        color = 'green';
+    }
+    if (count < 0) {
+        color = 'red';
+    }
+    if (count === 0) {
+        color = 'black';
+    }
+
+    const subtract = () => setCount(count - 1);
+    const reset = () => setCount(0);
+    const add = () => setCount(count + 1);
+
+    return (
+        <div className="container">
+        <h1 className="app_name">Counter App</h1>
+        <span style={{color}} className="number" id="number">{count}</span>
+        <div className="button-panel">
+            <button onClick={subtract} className="subtract" id="subtract">SUBTRACT</button>
+            <button onClick={reset} className="reset" id="reset">RESET</button>
+            <button onClick={add} className="add" id="add">ADD</button>
+        </div>
     </div>
-  );
+    )
+    
 }
 
 export default App;
